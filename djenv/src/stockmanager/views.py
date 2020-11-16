7308c969c4bc04fdf8c066fc072ad94b7bebcffd
Ugochukwu Anajemba <anajembu@gmail.com>
2020-11-16 09:57:43 +0000

    first commit

---
+++ djenv/src/stockmanager/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from .models import *
+from .forms import StockCreateForm
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def home(request):
+	title = 'Welcome: This is the Home Page'
+	context = {
+		"title": title,
+	}
+	return render(request, "home.html",context)
+
+def list_items(request):
+	title = 'List of Stock'
+	queryset = Stock.objects.all()
+	context = {
+		"title": title,
+		"queryset": queryset,
+	}
+	return render(request, "list_items.html",context)
+
+def add_items(request):
+	form = StockCreateForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('/list_items')
+	context = {
+		"form": form,
+		"title": "Add Item",
+	}
+	return render(request, "add_items.html", context)
